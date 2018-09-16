656841d848f919a97762930697bb15ad228f209d
smarotta <st.marotta@gmail.com>
2018-09-15 22:38:43 -0300

    Added unity client; Protobuf csharp client support

--- com.sm.mmo.moba.network/src/main/resources/protobuf/build-cs.bat
+++ com.sm.mmo.moba.network/src/main/resources/protobuf/build-cs.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET PWD=%cd%\
-SET DST_DIR=..\..\..\..\..\unity-csharp\
+SET DST_DIR=..\..\..\..\..\unity-client\Moba Client\Assets\Sources\Network
 SET SRC_DIR=messages\
 if not exist "%DST_DIR%" mkdir %DST_DIR%
 
-for /r %%v in (%SRC_DIR%*.proto) do protoc --proto_path=%PWD%%SRC_DIR% --csharp_out=%PWD%%DST_DIR% %%v
+for /r %%v in (%SRC_DIR%*.proto) do protoc --proto_path=%PWD%%SRC_DIR% --csharp_out="%PWD%%DST_DIR%" %%v
---
+++ unity-client/Moba Client/Assets/Sources/Hello.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Protobuf;
+using Messages;
+using System.IO;
+using System;
+
+public class Hello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Entity entity = new Entity() {
+            Hp = 1,
+            Level = 11,
+            Mana = 95,
+            Name = "Manolim 1",
+            Race = 1,
+            Type = 1,
+        };
+
+        string output = BitConverter.ToString(entity.ToByteArray());
+        Debug.Log("OUTPUT: " + output);
+
+        //MemoryStream ms = new MemoryStream();
+        //entity.WriteTo(new CodedOutputStream(ms));
+        //Debug.Log(ms.ToString());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
